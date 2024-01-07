a636a4bc226caefc839fb65119a56193d165449f
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-07 22:49:27 +0530

    working with complex JSX

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,20 @@
 function Products() {
-    return <h1>All Products</h1>
+  return (
+    <ul>
+      <li>
+        <div>
+          <div>
+            <h5>Fresh Milk</h5>
+            <p>sdfsf sdff sfdfsd fssd</p>
+            <div>
+              <h6>$111.00</h6>
+            </div>
+          </div>
+          <img src="" width="200"></img>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default Products;
