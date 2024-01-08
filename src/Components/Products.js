e5b2090fa0790f9ae6073b2e12c165b72f1b2bee
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-08 15:49:47 +0530

    working with JSX Attributes

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,9 @@
 let productCount = 0;
+let imageUrl = require("../images/test-img.jpg");
+// let style = {
+//     padding: '0px 20px',
+//     fontSize: 14
+// }
 
 function displayFormattedProductCount() {
     return productCount > 0 ? productCount : 'Zero';
@@ -13,13 +18,13 @@ function Products() {
             <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
             <p className="font-italic text-muted mb-0 small">sdfsf sdff sfdfsd fssd</p>
             <div className="d-flex align-items-center justify-content-start mt-1">
-              <h6 className="font-weight-bold my-2">$111.00</h6>
+              <h6 className="font-weight-bold my-2" style={ {'margin-right': 30} }>$111.00</h6>
               <button className="btn btn-primary">-</button>
-              <span>{displayFormattedProductCount()}</span>
+              <span style={ {padding: '0px 20px', 'font-size': 14} }>{displayFormattedProductCount()}</span>
               <button className="btn btn-primary">+</button>
             </div>
           </div>
-          <img src="" alt="Generic placeholder image" width="200" className="ml-lg-5 order-1 order-lg-2"></img>
+          <img src={imageUrl} alt="Generic placeholder image" width="100" className="ml-lg-5 order-1 order-lg-2"></img>
         </div>
       </li>
     </ul>
