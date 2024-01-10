7cf5dbbc28fc5eb974aee4409e0f5a02724c86e0
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-10 12:42:27 +0530

    using event handlers: react state & events

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,25 +1,34 @@
 import React from "react";
 import Button from "./Button";
 
-let productCount = 0;
-function displayFormattedProductCount() {
-  return productCount > 0 ? productCount : "Zero";
-}
-
 function ProductDetails(props) {
   let badgeClass = "badge-margin-left-240 badge ";
   badgeClass += props.isAvailable ? "bg-success" : "bg-danger";
+
+  let productCount = 0;
+  function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : "Zero";
+  }
+
+  let incrementProductCount = function () {
+    productCount++;
+    console.log(productCount);
+  };
+  let decrementProductCount = function () {
+    productCount--;
+    console.log(productCount);
+  };
 
   return (
     <div className="d-flex align-items-center justify-content-start mt-1">
       <h6 className="font-weight-bold my-2" style={{ "margin-right": 30 }}>
         ${props.price}
       </h6>
-      <Button>-</Button>
+      <Button eventHandler={decrementProductCount}>-</Button>
       <span style={{ padding: "0px 20px", "font-size": 14 }}>
         {displayFormattedProductCount()}
       </span>
-      <Button>+</Button>
+      <Button eventHandler={incrementProductCount}>+</Button>
       <span className={badgeClass}>
         {props.isAvailable ? "Available" : "UnAvailable"}
       </span>
