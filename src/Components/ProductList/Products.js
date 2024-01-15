63fff7a761fcd0c6396613a344851cea37f660a7
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-15 23:01:07 +0530

    dynamic inline styling in react

--- src/Components/ProductList/Products.js
+++ src/Components/ProductList/Products.js
@@ -12,7 +12,10 @@ import ProductDetails from "./ProductDetails";
 function Products(props) {
   console.log("Products Component Executed");
   return (
-    <li className="list-group-item">
+    <li
+      className="list-group-item"
+      style={{ backgroundColor: props.isAvailable ? "white" : "#dedede" }}
+    >
       <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
         <div className="media-body order-2 order-lg-1">
           <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
